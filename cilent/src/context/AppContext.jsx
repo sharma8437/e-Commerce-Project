74099da7408003b9ffe39f792105cc54cec28b2e
nikesh-avua <nikesh.sharma@avua.com>
2025-05-14 00:10:14 +0530

    setup the frontend folder structure

---
+++ cilent/src/context/AppContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, use, useContext, useState } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState(null)
+    const [isSeller, setIsSeller] = useState(false)
+
+;    const value ={navigate, user, setUser, isSeller, setIsSeller}
+  return <AppContext.Provider value={{value}}>
+      {children}
+    </AppContext.Provider>
+  
+
+}
+
+export const useAppContext = () =>{
+    return useContext(AppContext)
+}
